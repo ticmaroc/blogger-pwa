e47447a5ac9f70a0acf3cc615f9146ad35f46d97
ticmaroc <oumouy@yahoo.fr>
2025-12-11 21:33:47 +0100

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,39 +2,39 @@ import type { Config } from './types';
 
 export default {
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'Tic Maroc',
+  shortName: 'Tic Maroc',
+  description: 'Actualité des Tic au Maroc et ailleurs',
   direction: 'auto',
-  language: 'en-US',
+  language: 'fr-FR',
   backgroundColor: '#fff',
   themeColor: '#fff',
   display: 'standalone',
-  orientation: 'natural',
+  orientation: 'any',
   scope: '/',
   startUrl: '/?utm_source=homescreen',
   appleStatusBarStyle: 'black-translucent',
   preferRelatedApplications: false,
   shortcuts: [
     {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
+      name: 'Actualité',
+      shortName: 'Actualité',
+      description: 'Les articles du site',
+      url: '/search/label/Actualité?max-results=6&utm_source=homescreen',
     },
     {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      name: 'Test de débit',
+      shortName: 'Test de débit',
+      description: 'Test de débit internet',
+      url: '/p/test-debit.html?utm_source=homescreen',
     },
   ],
   pwa: {
     logs: true,
     // OneSignal is only available if you are using cloudflare workers
-    oneSignalEnabled: false, // To enable OneSignal, set this to true
+    oneSignalEnabled: true, // To enable OneSignal, set this to true
     oneSignalConfig: {
-      appId: '********-****-****-****-************', // Replace with your OneSignal App Id
+      appId: '7518ec8b-a0de-4af4-8691-7cc06260096f', // Replace with your OneSignal App Id
       allowLocalhostAsSecureOrigin: true,
     },
   },
